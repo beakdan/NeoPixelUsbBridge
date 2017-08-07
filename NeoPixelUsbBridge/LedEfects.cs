f8e5b6f66dbda23dc2c8eb36102bf9b40db847c8
beakdan <beakdan@gmail.com>
2017-08-07 07:31:13 -0500

    Add files via upload

---
+++ NeoPixelUsbBridge/LedEfects.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace NeoPixelUsbBridge
+{
+	class LedEfects
+	{
+		public static int[] sinColorTable = new int[128];
+
+		static LedEfects()
+		{
+			FillColorTable();
+		}
+
+		public class ColorChase
+		{
+			private int _blockCount;
+			private int _blockLenght;
+			private int _position;
+			private int _color;
+			private int[] pixels; 
+
+			public ColorChase(int LedCount, int Blocks, int BlockLength)
+			{
+				pixels = new int[LedCount];
+				_blockCount = Blocks;
+				_blockLenght = BlockLength;
+			}
+
+			public int[] MoveNext()
+			{
+				//Clear all the pixels
+				for (int i = 0; i < pixels.Length; i++)
+					pixels[i] = 0;
+
+				for (int b = 0; b < _blockCount; b++)
+				{
+					var blockIndex = ((pixels.Length / _blockCount) * b) + _position;
+					for (int i = 0; i < _blockLenght; i++)
+					{
+						var pixIndex = (blockIndex + i) % pixels.Length;
+						var colorIndex = (_color + i) % LedEfects.sinColorTable.Length;
+						pixels[pixIndex] = LedEfects.sinColorTable[colorIndex];
+					}
+				}
+
+				_position = (_position + 1) % pixels.Length;
+				_color = (_color + 1) % LedEfects.sinColorTable.Length;
+				return pixels;
+			}
+		}
+
+		private static void FillColorTable()
+		{
+			var refValues = new byte[sinColorTable.Length];
+			var phase = 120d;
+			//The on time for a color is 180°, the full cycle is phase * 3
+			var onTimeRatio = refValues.Length * 180d / (phase * 3);
+			var radian = Math.PI / onTimeRatio;
+
+			//compute the values to do a simple lookup
+			for (int i = 0; i < refValues.Length; i++)
+			{
+				//Console.WriteLine(Math.Sin(radian * i) * 255);
+				refValues[i] = (byte)(i < (int)onTimeRatio ? Math.Sin(radian * i) * 255 : 0);
+			}
+			//now asign each color with an offset of 120°
+			for (int i = 0; i < sinColorTable.Length; i++)
+			{
+				var greenOffset = (int)(i + (onTimeRatio * phase * 2 / 180d)) % sinColorTable.Length;
+				var blueOffset = (int)(i + (onTimeRatio * phase / 180d)) % sinColorTable.Length;
+				sinColorTable[i] =
+					(refValues[i] << 8) |				//red 
+					(refValues[greenOffset] << 16) |	//green
+					refValues[blueOffset];				//blue
+			}
+		}
+	}
+}
---
+++ NeoPixelUsbBridge/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace NeoPixelUsbBridge
+{
+	class Program
+	{
+		const int BYTESPERPIXEL = 8;
+		static string commPort = "COM11";
+		static int baudRate = 3000000;
+		static int pixelCount = 32;
+
+		static readonly byte[] bitTriplets = new byte[]
+		{
+			0x5b, 0x1b, 0x53, 0x13,
+			0x5a, 0x1a, 0x52, 0x12
+		};
+
+		static void Main(string[] args)
+		{
+			var colorBuffer = new int[pixelCount];
+			var uartBuffer = new byte[pixelCount * BYTESPERPIXEL];
+
+			Console.WriteLine("Bytes to send: {0}", uartBuffer.Length);
+
+			using (var serialPort = new SerialPort(commPort, baudRate, Parity.None, 7, StopBits.One))
+			{
+				if (!serialPort.IsOpen)
+					serialPort.Open();
+
+				var chase = new LedEfects.ColorChase(pixelCount, 3, 6);
+
+				for (;;)
+				{
+					TranslateColors(chase.MoveNext(), uartBuffer);
+					serialPort.BaseStream.Write(uartBuffer, 0, uartBuffer.Length);
+					serialPort.BaseStream.Flush();
+					Thread.Sleep(25);
+					if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.X)
+						break;
+				}
+			}
+		}
+
+		static void TranslateColors(int[] colors, byte[] UartData)
+		{
+			for (int i = 0; i < colors.Length; i++)
+			{
+				var color = colors[i];
+				var pixOffset = i * BYTESPERPIXEL;
+				
+				//only 8 permutations so no need to use a for loop
+				UartData[pixOffset] = bitTriplets[(color >> 21) & 0x07];
+				UartData[pixOffset + 1] = bitTriplets[(color >> 18) & 0x07];
+				UartData[pixOffset + 2] = bitTriplets[(color >> 15) & 0x07];
+				UartData[pixOffset + 3] = bitTriplets[(color >> 12) & 0x07];
+				UartData[pixOffset + 4] = bitTriplets[(color >> 9) & 0x07];
+				UartData[pixOffset + 5] = bitTriplets[(color >> 6) & 0x07];
+				UartData[pixOffset + 6] = bitTriplets[(color >> 3) & 0x07];
+				UartData[pixOffset + 7] = bitTriplets[color & 0x07];
+			}
+		}
+	}
+}
